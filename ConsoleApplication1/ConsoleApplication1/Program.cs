2cce5aafefd53c6715a26292e078a07c885d824d
Bohdan Soloviov <bs@pawa.ai>
2024-03-19 23:01:10 +0200

    init

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: dotnet run -p YourProjectName.csproj <path_to_cshtml>");
+                return;
+            }
+
+            string cshtmlFilePath = args[0];
+            ConvertCshtmlToHtml(cshtmlFilePath);
+        }
+
+        public static void ConvertCshtmlToHtml(string cshtmlFilePath)
+        {
+            if (File.Exists(cshtmlFilePath))
+            {
+                string cshtmlContent = File.ReadAllText(cshtmlFilePath);
+                string htmlFilePath = Path.ChangeExtension(cshtmlFilePath, ".html");
+                File.WriteAllText(htmlFilePath, cshtmlContent);
+
+                Console.WriteLine("Conversion successful!");
+            }
+            else
+            {
+                Console.WriteLine($"File {cshtmlFilePath} not found.");
+            }
+        }
+    }
+
+}
