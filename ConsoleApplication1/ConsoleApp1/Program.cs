6ca9668970900eb0988f4774f1f70891f29d8346
Bohdan Soloviov <bs@pawa.ai>
2024-03-19 23:54:30 +0200

    01

--- ConsoleApplication1/ConsoleApp1/Program.cs
+++ ConsoleApplication1/ConsoleApp1/Program.cs
@@ -1,28 +1,30 @@
 ﻿using System;
 using System.IO;
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        if (args.Length == 0)
+        if (args.Length < 2)
         {
-            Console.WriteLine("Usage: dotnet run -p YourProjectName.csproj <path_to_cshtml>");
+            Console.WriteLine("Usage: dotnet run -p YourProjectName.csproj <path_to_cshtml> <output_directory>");
             return;
         }
 
         string cshtmlFilePath = args[0];
-        string htmlFilePath = args[1];
-        ConvertCshtmlToHtml(cshtmlFilePath,htmlFilePath);
+        string outputDirectory = args[1];
+        ConvertCshtmlToHtml(cshtmlFilePath, outputDirectory);
     }
 
-    public static void ConvertCshtmlToHtml(string cshtmlFilePath,string htmlFilePath)
+    public static void ConvertCshtmlToHtml(string cshtmlFilePath, string outputDirectory)
     {
         if (File.Exists(cshtmlFilePath))
         {
             string cshtmlContent = File.ReadAllText(cshtmlFilePath);
+            string htmlFilePath = Path.Combine(outputDirectory, "index.html");
             File.WriteAllText(htmlFilePath, cshtmlContent);
 
-            Console.WriteLine("Conversion successful! :"+htmlFilePath+"sc:"+cshtmlContent);
+            Console.WriteLine("Conversion successful! HTML file created at: " + htmlFilePath);
         }
         else
         {
